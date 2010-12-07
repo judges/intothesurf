5a6142cfa66ea744af6fd8f7e27ee2d2ea0036b5
lotionboy <t2mann@gmail.com>
2010-12-07 13:27:27 -0700

    adding data to sponsors

--- RaceInformationCreator.m
+++ RaceInformationCreator.m
@@ -31,15 +31,15 @@
 	dayArray =[[[NSMutableArray alloc]init] autorelease];
 
 	eventArray=[[[NSMutableArray alloc]init] autorelease];
-	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"Lavaman 75% Refund - last day to request refund." Location:@"Lavaman" TimeFrom:[dateFormatter dateFromString:@"2011-03-01 0:00:00"] TimeTo:nil]];	
+	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"Lavaman 75% Refund - last day to request refund." Location:@"Lavaman" TimeFrom:[dateFormatter dateFromString:nil] TimeTo:nil]];	
 	[dayArray addObject:[RaceDay RaceDayWithTitle:@"Saturday" Date:[dateFormatter dateFromString:@"2011-01-01 0:00:00"]  Events:eventArray]];
 	
 	eventArray=[[[NSMutableArray alloc]init] autorelease];
-	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"Lavaman 50% Refund - last day to request refund." Location:@"Lavaman" TimeFrom:[dateFormatter dateFromString:@"2011-01-01 0:00:00"] TimeTo:nil]];	
+	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"Lavaman 50% Refund - last day to request refund." Location:@"Lavaman" TimeFrom:[dateFormatter dateFromString:nil] TimeTo:nil]];	
 	[dayArray addObject:[RaceDay RaceDayWithTitle:@"Monday" Date:[dateFormatter dateFromString:@"2011-02-28 0:00:00"]  Events:eventArray]];
 	
 	eventArray=[[[NSMutableArray alloc]init] autorelease];
-	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"LavaKids" Location:@"Anaehoomalu Bay" TimeFrom:[dateFormatter dateFromString:@"2011-04-02 8:00:00"] TimeTo:nil]];	
+	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"LavaKids" Location:@"Anaehoomalu Bay" TimeFrom:[dateFormatter dateFromString:@"2011-04-02 8:00:00"] TimeTo:@"2011-04-02 9:00:00"]];	
 	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"Packet Pickup" Location:@"Hilton Waikoloa" TimeFrom:[dateFormatter dateFromString:@"2011-04-02 13:00:00"] TimeTo:nil]];	
 	[eventArray addObject:[RaceEvent RaceEventWithTitle:@"Pre-Race Meeting" Location:@"Hilton Waikoloa" TimeFrom:[dateFormatter dateFromString:@"2011-04-02 16:30:00"] TimeTo:nil]];		
 	[dayArray addObject:[RaceDay RaceDayWithTitle:@"Saturday" Date:[dateFormatter dateFromString:@"2011-04-02 0:00:00"]  Events:eventArray]];
--- SponsorsCreator.m
+++ SponsorsCreator.m
@@ -16,19 +16,19 @@
 {
 	NSMutableArray* itemArray = [[NSMutableArray alloc]init];
 	
-	[itemArray addObject:[Item initWithTitle:@"McDonalds" Image:@"mcdonalds.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
-	[itemArray addObject:[Item initWithTitle:@"Hilton" Image:@"hilton.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
-	[itemArray addObject:[Item initWithTitle:@"Holiday Inn" Image:@"HolidayInn.JPG" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	[itemArray addObject:[Item initWithTitle:@"Starbucks" Image:@"starbucks.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
-	[itemArray addObject:[Item initWithTitle:@"Coca-Cola" Image:@"coca_cola.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	[itemArray addObject:[Item initWithTitle:@"Oomph!" Image:@"oomph.png" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	[itemArray addObject:[Item initWithTitle:@"Coca-Cola" Image:@"coca_cola.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	[itemArray addObject:[Item initWithTitle:@"Hammer" Image:@"hammer.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	[itemArray addObject:[Item initWithTitle:@"HawaiiPhotoman" Image:@"hawaiiphotoman.jpg" Address:@"Dayton, Ohio" Info:@"Promotion: 5% off" Link:@"" ll1:39.75049 ll2:-84.193726]];
-	[itemArray addObject:[Item initWithTitle:@"Kona Brewery" Image:@"konabrew.jpg" Address:@"75-5629 Kuakini Highway, Kailua-Kona, Hi 96740" Info:@"Promotion: 5% off" Link:@"http://konabrewingco.com" ll1:19.641961 ll2:-155.996461]];
-	[itemArray addObject:[Item initWithTitle:@"XTerra" Image:@"xterra.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	
-	
+	[itemArray addObject:[Item initWithTitle:@"Hilton" Image:@"hilton.jpg" Address:@"425 Waikoloa Beach Drive, Waikoloa, HI 96738" Info:@"Promotion: " Link:@"http://www.hiltonwaikoloavillage.com/" ll1:39.75049 ll2:-84.193726]];	
+	[itemArray addObject:[Item initWithTitle:@"Waikoloa Land Co." Image:@"hawaiiphotoman.jpg" Address:@"Kailua Kona, Hi 96740" Info:@"Promotion: " Link:@"http://www.waikoloabeachresort.com/" ll1:39.75049 ll2:-84.193726]];
+	[itemArray addObject:[Item initWithTitle:@"XTerra" Image:@"xterra.jpg" Address:@"http://www.xterrawetsuits.com/" Info:@"Promotion: " Link:@"http://www.xterrawetsuits.com/"]];
+	[itemArray addObject:[Item initWithTitle:@"Hammer" Image:@"hammer.jpg" Address:@"http://www.hammernutrition.com/" Info:@"Promotion: " Link:@"http://www.hammernutrition.com/"]];
+	[itemArray addObject:[Item initWithTitle:@"Kona Brewery" Image:@"konabrew.jpg" Address:@"75-5629 Kuakini Highway, Kailua-Kona, Hi 96740" Info:@"Promotion: " Link:@"http://konabrewingco.com" ll1:19.641961 ll2:-155.996461]];
+	[itemArray addObject:[Item initWithTitle:@"HawaiiPhotoman" Image:@"hawaiiphotoman.jpg" Address:@"Kailua Kona, Hi 96740" Info:@"Promotion: " Link:@"http://www.hawaiiphotoman.com/" ll1:39.75049 ll2:-84.193726]];
+	[itemArray addObject:[Item initWithTitle:@"Bike Works" Image:@"bikeworks.jpg" Address:@"74-5583 Luhia Street, Kailua Kona, Hi 96740" Info:@"Promotion:" Link:@"http://www.bikeworkskona.com/" ll1:39.75049 ll2:-84.193726]];
+	//[itemArray addObject:[Item initWithTitle:@"McDonalds" Image:@"mcdonalds.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
+	//[itemArray addObject:[Item initWithTitle:@"Holiday Inn" Image:@"HolidayInn.JPG" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
+	//[itemArray addObject:[Item initWithTitle:@"Starbucks" Image:@"starbucks.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
+	//[itemArray addObject:[Item initWithTitle:@"Coca-Cola" Image:@"coca_cola.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
+	//[itemArray addObject:[Item initWithTitle:@"Oomph!" Image:@"oomph.png" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
+	//[itemArray addObject:[Item initWithTitle:@"Coca-Cola" Image:@"coca_cola.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
 	
 	return itemArray;
 }
