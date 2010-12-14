434fc34f04f1ac785fa35a3dc077b9fb7b94b1f5
lotionboy <t2mann@gmail.com>
2010-12-14 00:46:13 -0700

    lots of updates to data files

--- SponsorsCreator.m
+++ SponsorsCreator.m
@@ -16,20 +16,21 @@
 {
 	NSMutableArray* itemArray = [[NSMutableArray alloc]init];
 	
-	[itemArray addObject:[Item initWithTitle:@"Hilton"            Image:@"hilton.jpg"         Address:@"425 Waikoloa Beach Drive, Waikoloa, HI 96738"   Info:@"Promotion: " Link:@"www.hiltonwaikoloavillage.com" ll1:19.92353  ll2:-155.88728]];	
-	[itemArray addObject:[Item initWithTitle:@"Waikoloa Land Co." Image:@""                   Address:@"Kailua Kona, Hi 96740"                          Info:@"Promotion: " Link:@"www.waikoloabeachresort.com"   ll1:19.92353  ll2:-155.88728]];
-	[itemArray addObject:[Item initWithTitle:@"XTerra"            Image:@"xterra.jpg"         Address:@"www.xterrawetsuits.com"                         Info:@"Promotion: " Link:@"www.xterrawetsuits.com"        ll1:19.92353  ll2:-155.88728]];
-	[itemArray addObject:[Item initWithTitle:@"Hammer"            Image:@"hammer.jpg"         Address:@"www.hammernutrition.com"                        Info:@"Promotion: " Link:@"www.hammernutrition.com"       ll1:19.92353  ll2:-155.88728]];
-	[itemArray addObject:[Item initWithTitle:@"Kona Brewery"      Image:@"konabrew.jpg"       Address:@"75-5629 Kuakini Highway, Kailua-Kona, Hi 96740" Info:@"Promotion: " Link:@"www.konabrewingco.com"         ll1:19.641961 ll2:-155.996461]];
-	[itemArray addObject:[Item initWithTitle:@"HawaiiPhotoman"    Image:@"hawaiiphotoman.jpg" Address:@"Kailua Kona, Hi 96740"                          Info:@"Promotion: " Link:@"www.hawaiiphotoman.com"        ll1:19.64273  ll2:-155.99721]];
-	[itemArray addObject:[Item initWithTitle:@"Bike Works"        Image:@"bikeworks.jpg"      Address:@"74-5583 Luhia Street, Kailua Kona, Hi 96740"    Info:@"Promotion: " Link:@"www.bikeworkskona.com"         ll1:19.64756  ll2:-155.99867]];
+	[itemArray addObject:[Item initWithTitle:@"Hilton Waikolo"     Image:@"hilton.jpg"         Address:@"425 Waikoloa Beach Drive, Waikoloa, HI"   Info:@"Promotion: " Link:@"www.hiltonwaikoloavillage.com" ll1:19.92353  ll2:-155.88728]];	
+	[itemArray addObject:[Item initWithTitle:@"Queens MarketPlace" Image:@"queens.jpg"         Address:@"xxx"                                            Info:@"Promotion: " Link:@"www.xxx.com" ll1:0  ll2:-155.98135]]; 
 
-	//[itemArray addObject:[Item initWithTitle:@"McDonalds" Image:@"mcdonalds.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
-	//[itemArray addObject:[Item initWithTitle:@"Holiday Inn" Image:@"HolidayInn.JPG" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	//[itemArray addObject:[Item initWithTitle:@"Starbucks" Image:@"starbucks.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 10% off" Link:@""]];	
-	//[itemArray addObject:[Item initWithTitle:@"Coca-Cola" Image:@"coca_cola.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	//[itemArray addObject:[Item initWithTitle:@"Oomph!" Image:@"oomph.png" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
-	//[itemArray addObject:[Item initWithTitle:@"Coca-Cola" Image:@"coca_cola.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: 5% off" Link:@""]];
+	[itemArray addObject:[Item initWithTitle:@"XTerra Wetsuits"    Image:@"xterra.jpg"         Address:@"www.xterrawetsuits.com"                         Info:@"Promotion: " Link:@"www.xterrawetsuits.com"        ll1:32.88040  ll2:-117.18359]]; 
+	[itemArray addObject:[Item initWithTitle:@"Hammer Nutrition"   Image:@"hammer.jpg"         Address:@"www.hammernutrition.com"                        Info:@"Promotion: " Link:@"www.hammernutrition.com"       ll1:0  ll2:-155.88728]];
+	[itemArray addObject:[Item initWithTitle:@"Bike Works"         Image:@"bikeworks.jpg"      Address:@"74-5583 Luhia Street, Kailua Kona, Hi"    Info:@"Promotion: " Link:@"www.bikeworkskona.com"         ll1:19.64756  ll2:-155.99867]];
+
+	[itemArray addObject:[Item initWithTitle:@"HawaiiPhotoman"     Image:@"hawaiiphotoman.jpg" Address:@"Kailua Kona, Hi 96740"                          Info:@"Promotion: " Link:@"www.hawaiiphotoman.com"        ll1:19.64273  ll2:-155.99721]];
+
+	[itemArray addObject:[Item initWithTitle:@"Kona Brewery"       Image:@"konabrew.jpg"       Address:@"75-5629 Kuakini Hwy, Kailua-Kona, Hi"               Info:@"Promotion: " Link:@"www.konabrewingco.com"         ll1:19.641961 ll2:-155.996461]];
+	[itemArray addObject:[Item initWithTitle:@"Jackie Reys"	       Image:@"jackiereys.jpg"     Address:@"75-5995 Kuakini Hwy., Kailua-Kona, Hawaii 808.327.0209" Info:@"Promotion: " Link:@"www.jackiereys.com"            ll1:19.62707  ll2:-155.98135]];	
+	[itemArray addObject:[Item initWithTitle:@"Starbucks"	       Image:@"starbucks.jpg"      Address:@"201 Waikoloa Beach Dr, Waikoloa, HI"                          Info:@"Promotion: " Link:@"www.starbucks.com"             ll1:19.91370  ll2:-155.87999]];	
+ 
+	[itemArray addObject:[Item initWithTitle:@"Fair Wind Cruises"  Image:@"fairwind.jpg"       Address:@"78-7130 Kaleiopapa Street, Kailua-Kona, HI 808.322.2788"      Info:@"Promotion: " Link:@"www.fair-wind.com"             ll1:19.56177  ll2:-155.96215]];
+	[itemArray addObject:[Item initWithTitle:@"Ocean Sports"	   Image:@"oceansports.jpg"    Address:@"69-275 Waikoloa Beach Drive, Kailua-Kona HI"                  Info:@"Promotion: " Link:@"www.hawaiioceansports.com"     ll1:19.91697  ll2:-155.88473]];
 	
 	return itemArray;
 }
--- TravelActivitiesCreator.m
+++ TravelActivitiesCreator.m
@@ -18,49 +18,49 @@
 	NSMutableArray *array =[[NSMutableArray alloc]init];
 		
 	// food
-	[itemArray addObject:[Item initWithTitle:@"Kona Brewery" Image:@"konabrew.jpg"  Address:@"23 str, Kailua-Kona" Info:@"Promotion: " Link:@"www.konabrewery.com"]];	
-	[itemArray addObject:[Item initWithTitle:@"Starbucks"    Image:@"starbucks.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: " Link:@"www.starbucks.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Kona Brewery" Image:@"konabrew.jpg"   Address:@"23 str, Kailua-Kona" Info:@"Promotion: coming soon" Link:@"www.konabrewery.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Jackie Reys"  Image:@"jackiereys.jpg" Address:@"75-5995 Kuakini Highway, Kailua-Kona, HI" Info:@"Promotion: coming soon" Link:@"www.jackiereys.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Starbucks"    Image:@"starbucks.jpg"  Address:@"23 str, Kailua-Kona" Info:@"Promotion: coming soon" Link:@"www.starbucks.com"]];	
 	[array addObject:[Category categoryWithImageName:@"food_category.png" Label:@"Food" Items:itemArray]];
 	[itemArray release];
 	
 	//activities
 	itemArray =[[NSMutableArray alloc]init];
-	[itemArray addObject:[Item initWithTitle:@"Coming Soon" Image:@"comingsoon.jpg" Address:@"lavamantriathlon.com" Info:@"Promotion: " Link:@"www.lavamantriathlon.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Fair Wind Cruises" Image:@"fairwind.jpg" Address:@"78-7130 Kaleiopapa Street, Kailua-Kona" Info:@"Promotion: coming soon" Link:@"www.fair-wind.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Ocean Sports"      Image:@"fairwind.jpg" Address:@"69-275 Waikoloa Beach Drive HI 96738" Info:@"Promotion: coming soon" Link:@"hawaiioceansports.com"]];		
 	[array addObject:[Category categoryWithImageName:@"activities_category.png" Label:@"Activities" Items:itemArray]];
 	[itemArray release];
 	
 	//shopping
 	itemArray =[[NSMutableArray alloc]init];
-	[itemArray addObject:[Item initWithTitle:@"Coming Soon" Image:@"comingsoon.jpg" Address:@"lavamantriathlon.com" Info:@"Promotion: " Link:@"www.lavamantriathlon.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Queens Marketplace" Image:@"comingsoon.jpg" Address:@"lavamantriathlon.com" Info:@"Promotion: " Link:@"www.lavamantriathlon.com"]];	
 	[array addObject:[Category categoryWithImageName:@"shopping_category.png" Label:@"Shopping" Items:itemArray]];
 	[itemArray release];
 	
 	//places
 	itemArray =[[NSMutableArray alloc]init];
-	[itemArray addObject:[Item initWithTitle:@"Coming Soon" Image:@"comingsoon.jpg" Address:@"lavamantriathlon.com" Info:@"Promotion: " Link:@"www.lavamantriathlon.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Coming Soon"     Image:@"comingsoon.jpg" Address:@"lavamantriathlon.com" Info:@"Promotion: " Link:@"www.lavamantriathlon.com"]];	
 	[array addObject:[Category categoryWithImageName:@"places_category.png" Label:@"Sites" Items:itemArray]];
 	[itemArray release];
 	
 	//hotels
 	itemArray =[[NSMutableArray alloc]init];
-	[itemArray addObject:[Item initWithTitle:@"Hilton"      Image:@"hilton.jpg"     Address:@"75-5919 Alii Dr., Kailua-Kona" Info:@"Promotion: " Link:@"www.hilton.com"]];	
-	[itemArray addObject:[Item initWithTitle:@"Holiday Inn" Image:@"HolidayInn.JPG" Address:@"description"                   Info:@"Promotion: " Link:@"www.holidayinn.com"]];
-	[itemArray addObject:[Item initWithTitle:@"Marriott"    Image:@"marriott.png"   Address:@"description"                   Info:@"Promotion: " Link:@"www.marriott.com"]];
+	[itemArray addObject:[Item initWithTitle:@"Hilton"          Image:@"hilton.jpg"     Address:@"75-5919 Alii Dr., Kailua-Kona" Info:@"Promotion: " Link:@"www.hilton.com"]];	
 	[array addObject:[Category categoryWithImageName:@"cart.jpg" Label:@"Lodging" Items:itemArray]];
 	[itemArray release];
 	
 	//gear
 	itemArray =[[NSMutableArray alloc]init];
-	[itemArray addObject:[Item initWithTitle:@"Bike Works" Image:@"bikeworks.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: " Link:@"www.bikeworks.com"]];	
-	[itemArray addObject:[Item initWithTitle:@"Hammer"     Image:@"hammer.jpg"    Address:@"23 str, Kailua-Kona" Info:@"Promotion: " Link:@"www.hammer.com"]];	
-	[itemArray addObject:[Item initWithTitle:@"XTerra"     Image:@"xterra.jpg"    Address:@"23 str, Kailua-Kona" Info:@"Promotion: " Link:@"www.xterrawetsuits.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Bike Works"      Image:@"bikeworks.jpg" Address:@"23 str, Kailua-Kona" Info:@"Promotion: " Link:@"www.bikeworks.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Hammer"          Image:@"hammer.jpg"    Address:@"4952 Whitefish Stage Rd, Whitefish, MT 59937" Info:@"Promotion: " Link:@"www.hammernutrition.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"XTerra Wetsuits" Image:@"xterra.jpg"    Address:@"8380 Miramar Mall, San Diego, CA 92121" Info:@"Promotion: " Link:@"www.xterrawetsuits.com"]];	
 	[array addObject:[Category categoryWithImageName:@"cart.jpg" Label:@"Gear" Items:itemArray]];
 	[itemArray release];
 	
 	//coaching
-	itemArray =[[NSMutableArray alloc]init];  
-	[itemArray addObject:[Item initWithTitle:@"Karolina Wanke"   Image:@"karolina.jpg" Address:@"75-135 Kamilo St., Kailua-Kona" Info:@"Promotion: " Link:@"www.intothesurf.com"]];	
-	[itemArray addObject:[Item initWithTitle:@"Luis De La Torre" Image:@"luis.jpg"     Address:@"75-135 Kamilo St., Kailua-Kona" Info:@"Promotion: " Link:@"www.intothesurf.com"]];	
+	itemArray =[[NSMutableArray alloc]init];
+	[itemArray addObject:[Item initWithTitle:@"Eric Neilsen"    Image:@"karolina.jpg" Address:@"Kailua-Kona, HI" Info:@"" Link:@"www.coachericneilsen.com"]];	
+	[itemArray addObject:[Item initWithTitle:@"Luis DeLa Torre" Image:@"luis.jpg"     Address:@"Kailua-Kona, HI" Info:@"" Link:@"www.luisdelatorre.com"]];	
 	[array addObject:[Category categoryWithImageName:@"karolina.jpg" Label:@"Coaching" Items:itemArray]];
 	[itemArray release];
 	
